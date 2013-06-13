4421a9de336a31e4e912c63214c526400539faab
Akshay <akshay.s@multunus.com>
2013-06-13 19:51:18 +0530

    added functionality to display questions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 gem "ProMotion", "~> 0.7.5"
 gem "teacup"
+gem "bubble-wrap", "~> 1.3.0"
 
 group :spec do
   gem "motion-facon"
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,12 @@
 describe "Delegate" do
   before do
     @delegate = TestDelegate.new
+    @app = UIApplication.sharedApplication
   end
 
-  it "open Questions Screen" do
-    view('Survey').should.not.be.nil
+  it "should have Questions Screen in navigations controller" do
+   window = @app.keyWindow
+   root_view_controller = window.rootViewController
+   root_view_controller.viewControllers.last.class.should == QuestionScreen
   end
 end
