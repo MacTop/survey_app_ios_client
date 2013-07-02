0e907454be8ac6a2c4561541cfe7bc918d558bc3
midhun krishna <midhun.k@multunus.com>
2013-07-02 11:28:16 +0530

    removed invalid puts statements

--- app/screens/survey_list/views/survey_list_item_view.rb
+++ app/screens/survey_list/views/survey_list_item_view.rb
@@ -4,7 +4,6 @@ class SurveyListItemView < SurveyListItemTemplate
     super
     self.userInteractionEnabled = true
     add_response_list_navigation
-    puts self.class
     color = {:red => 0.8, :green => 0.8, :blue => 0.8, :alpha => 1}
     apply_border_radius self, 2
     apply_box_shadow_to(self, color, 1, 0, CGSizeMake(-2,2), true)
