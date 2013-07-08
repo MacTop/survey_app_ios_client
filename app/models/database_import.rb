8fe080a32bd19857f4700c481be94411e4ce9d30
Vimal V Nair <vimalvnair999@gmail.com>
2013-07-08 17:06:39 +0530

    Loading test db from different resources

--- app/models/database_import.rb
+++ app/models/database_import.rb
@@ -1,25 +1,38 @@
 class DataStore
-
+  
   def self.get_data_for(resource)
     file_path = NSBundle.mainBundle.pathForResource(resource, ofType: "json")
     BW::JSON.parse(NSData.dataWithContentsOfFile file_path)
   end
 
   def self.import
+    set_resource_names
     DataStore.import_surveys
     DataStore.import_questions
     DataStore.import_options
   end
 
+  def self.set_resource_names
+    if RUBYMOTION_ENV == 'test'
+      @survey_data = "surveys_test"
+      @question_data = "questions_test"
+      @option_data = "options_test"
+    else
+      @survey_data = "surveys"
+      @question_data = "questions"
+      @option_data = "options"
+    end
+  end
+
   def self.import_surveys
-    get_data_for("surveys").each do |survey|
+    get_data_for(@survey_data).each do |survey|
       survey_model = Survey.new(:id => survey[:id], :name => survey[:name], :expiry_date => survey[:expiry_date], :description => survey[:description])
       survey_model.save
     end
   end
 
   def self.import_questions    
-    get_data_for("questions").each do |question|
+    get_data_for(@question_data).each do |question|
       survey = Survey.find(:id => question[:survey_id]).first
       survey.questions << Question.new(:id => question[:id], :survey_id => question[:survey_id], :content => question[:content], :type => question[:type], :mandatory => question[:mandatory], :parent_id => 0)
       survey.save
@@ -27,7 +40,7 @@ class DataStore
   end
 
   def self.import_options
-    get_data_for("options").each do |option|
+    get_data_for(@option_data).each do |option|
       question = Question.find(:id => option[:question_id]).first
       radio_option = RadioOption.new(:id => option[:id], :question_id => option[:question_id], :content => option[:content])
       option[:questions].each do |sub_question|
